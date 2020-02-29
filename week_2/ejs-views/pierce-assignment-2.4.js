74dbc3b9a2ec7271781bf96b2f5537e4fee12236
Kimberly <kpierce865@yahoo.com>
2020-02-29 17:42:00 -0500

    commit

--- week_2/ejs-views/pierce-assignment-2.4.js
+++ week_2/ejs-views/pierce-assignment-2.4.js
@@ -1,8 +1,8 @@
 /*
 ============================================
 ; Title: Assignment 2.4
-; Author: Kimberly Pierce
-; Date: 19 February 2020
+; Author: Professor Krasso
+; Date: 27 February 2020
 ; Modified By: Kimberly Pierce
 ; Description: EJS Views
 ;===========================================
@@ -16,15 +16,15 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
-app.set("views",path.resolve(__dirname,"views"));
-app.set('view engine', 'ejs');
-app.get("/", function(request,response){
+app.set("views",path.resolve(__dirname,"views"));//tell express where to find views
+app.set('view engine', 'ejs');//tell express to use ejs views
+app.get("/", function(request,response){//message response
   response.render("index",{
     message: "Kimberly Pierce, 12345 Main Drive Knoxville, TN "
   });
 });
 
-http.createServer(app).listen(8080, function(){
+http.createServer(app).listen(8080, function(){//start server on localhost
   console.log("EJS-Views app started on port 8080.");
 });
 
