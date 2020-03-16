d0f0ea8b93eeb26dd0813eeafa413b166fdfd7e1
Kimberly <kpierce865@yahoo.com>
2020-03-16 19:28:03 -0400

    commit

--- week_5/if-else-render/pierce-assignment-5.2.js
+++ week_5/if-else-render/pierce-assignment-5.2.js
@@ -11,28 +11,30 @@
 const header = require('../../pierce-header.js'); //import statement for header file
 console.log(header.display("Kimberly", "Pierce", "Assignment 5.2") + " \n ");
 
+//required modules
 var express = require ("express");
 var http = require ("http");
 var path = require("path");
 
 app = express();
-
+//find views
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 
+//create array
 var f = [
   "Molly",
   "Ben",
   "Swidgen",
   "Wee"
 ];
-
+//display index page with array
 app.get("/", function(request, response){
   response.render("index",{
     dogs:f
   });
 });
-
+//create server
 http.createServer(app).listen(8080, function(){
   console.log("Application started on port 8080");
 });
