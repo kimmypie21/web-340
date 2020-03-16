f7ef8056504b2783633874ea88a37f75d66da9e8
Kimberly <kpierce865@yahoo.com>
2020-03-16 19:45:24 -0400

    commit

--- week_5/pug-template/pierce-assignment-5.3.js
+++ week_5/pug-template/pierce-assignment-5.3.js
@@ -10,22 +10,23 @@
 
 const header = require('../../pierce-header.js'); //import statement for header file
 console.log(header.display("Kimberly", "Pierce", "Assignment 5.3") + " \n ");
-
+//required modules
 var express = require("express");
 var http = require("http");
 var path = require ("path");
 var pug = require ("pug");
 
 var app = express();
+//find views, use pug
 app.set ("views", path.resolve(__dirname, "views"));
 app.set ("view engine", "pug");
-
+//display index and message
 app.get("/", function(request, response){
   response.render("index",{
     message:"Pug is Great!"
   });
 });
-
+//start server
 http.createServer(app).listen(8080, function(){
   console.log("Application started on port 8080");
 });
