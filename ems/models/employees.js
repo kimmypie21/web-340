2f37b9ba9c6ca473bdbcb5d27a6d33ed231ba780
Kimberly <kpierce865@yahoo.com>
2020-04-01 22:17:36 -0400

    commit

---
+++ ems/models/employees.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title: Assignment 7.4
+; Author: Kimberly Pierce
+; Date: 1 April 2020
+; Modified By: Kimberly Pierce
+; Description: EMS Milestone 3
+;===========================================
+*/
+
+const header = require('../../pierce-header.js'); //import statement for header file
+console.log(header.display("Kimberly", "Pierce", "Assignment 7.4") + " \n ");
